ca6ecf4ed972428fa067282636c962576eafe2e8
Sina Rajabpour <sina.rajabpour@outlook.com>
2021-07-02 23:39:35 +0430

    first commit

---
+++ src/views/EventList.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>Events Listing</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "EventList"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/EventShow.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>Showing event number #1</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "EventShow"
+}
+</script>
+
+<style scoped>
+
+</style>
